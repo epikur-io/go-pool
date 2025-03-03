4a8b0a2968142bb1af1dec299aa5a250bd092503
Epikur <3p1kur@protonmail.ch>
2025-03-03 09:17:50 +0100

    more unit tests

--- pool_test.go
+++ pool_test.go
@@ -1,6 +1,7 @@
 package pool
 
 import (
+	"context"
 	"fmt"
 	"testing"
 	"time"
@@ -10,6 +11,13 @@ type poolItem any
 
 func poolFactory() *poolItem {
 	return nil
+}
+
+func TestFactoryFunc(t *testing.T) {
+	pool := NewPool(2, poolFactory)
+	if pool.FactoryFunc() == nil {
+		t.Errorf("missing factory function")
+	}
 }
 
 func TestAqcuireAndRelease(t *testing.T) {
@@ -39,6 +47,80 @@ func TestAqcuireAndRelease(t *testing.T) {
 	if plen != 2 {
 		t.Errorf("pool expected to be full but got %d instances", plen)
 	}
+
+	_, err = pool.AcquireWithTimeout(1 * time.Second)
+	if err != nil {
+		t.Errorf("unexpected error: %e", err)
+	}
+}
+
+func TestAqcuireAndReleaseWithContext(t *testing.T) {
+	pool := NewPool(2, poolFactory)
+	entries := []*poolItem{}
+	for range 2 {
+		entry := pool.Acquire()
+		entries = append(entries, entry)
+	}
+
+	plen := pool.Len()
+	if plen != 0 {
+		t.Errorf("pool expected to be empty but %d instances remained", plen)
+	}
+
+	// should timeout since pool is now empty
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	_, err := pool.AcquireWithContext(ctx)
+	cancel()
+	if err == nil {
+		t.Errorf("expected timout error but got %v", err)
+	}
+	for _, entry := range entries {
+		pool.Release(entry)
+	}
+
+	plen = pool.Len()
+	if plen != 2 {
+		t.Errorf("pool expected to be full but got %d instances", plen)
+	}
+
+	ctx, cancel = context.WithTimeout(context.Background(), 1*time.Second)
+	_, err = pool.AcquireWithContext(ctx)
+	cancel()
+	if err != nil {
+		t.Errorf("unexpected error: %e", err)
+	}
+}
+
+func TestTryRelease(t *testing.T) {
+	pool := NewPool(2, poolFactory)
+	err := pool.TryRelease(nil)
+	if err == nil {
+		t.Errorf("error expected but got %v", err)
+	}
+
+	entry := pool.Acquire()
+	err = pool.TryRelease(entry)
+	if err != nil {
+		t.Errorf("unexpected error: %e", err)
+	}
+}
+func TestTryReleaseWithContext(t *testing.T) {
+	pool := NewPool(2, poolFactory)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	err := pool.TryReleaseWithContext(ctx, nil)
+	cancel()
+	if err == nil {
+		t.Errorf("error expected but got %v", err)
+	}
+
+	entry := pool.Acquire()
+	ctx, cancel = context.WithTimeout(context.Background(), 1*time.Second)
+	cancel()
+	err = pool.TryReleaseWithContext(ctx, entry)
+	if err != nil {
+		t.Errorf("unexpected error: %e", err)
+	}
 }
 
 func TestUpdate(t *testing.T) {
@@ -56,7 +138,7 @@ func TestUpdate(t *testing.T) {
 		}
 	}()
 	start := time.Now()
-	pool.LockedRun(func(p *Pool[poolItem]) error {
+	if err := pool.LockedRun(func(p *Pool[poolItem]) error {
 		for i := 0; i < p.Cap(); i++ {
 			// empty the Pool
 			p.Acquire()
@@ -67,7 +149,9 @@ func TestUpdate(t *testing.T) {
 		}
 
 		return nil
-	})
+	}); err != nil {
+		t.Error(err)
+	}
 	duration := time.Since(start)
 
 	if duration < delay {
@@ -87,7 +171,7 @@ func TestUpdateTimeout(t *testing.T) {
 	updatedInstances := 0
 
 	tc := time.After(1 * time.Second)
-	pool.LockedRun(func(p *Pool[poolItem]) error {
+	if err := pool.LockedRun(func(p *Pool[poolItem]) error {
 		for i := 0; i < p.Cap(); i++ {
 			// empty the Pool
 			select {
@@ -109,7 +193,9 @@ func TestUpdateTimeout(t *testing.T) {
 		}
 
 		return nil
-	})
+	}); err == nil {
+		t.Errorf("error expected but got %v", err)
+	}
 
 	if removedInstances != 0 {
 		t.Errorf("expected %d removed instances but got %d", pool.Len(), removedInstances)
